c348e7cbd3e850a03fa0aefece5c60d2adcb541f
Gitea <gitea@fake.local>
2022-02-27 01:50:27 +0530

    Math

---
+++ src/com/company/math.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class math {
+
+    public static void calculations() {
+
+        System.out.println("Max of 5, 3: " + Math.max(3, 5));
+
+        System.out.println("Random: " + Math.random());
+
+        System.out.println("Random int: " + (int)(Math.random()*100));
+    }
+}
