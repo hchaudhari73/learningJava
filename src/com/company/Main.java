6da88bff3c121734344ab223eb01749044ca10ca
Gitea <gitea@fake.local>
2022-02-27 01:42:49 +0530

    Operators

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// variable
-    variables.primitives();
 
-    variables.nonPrimitive();
-
+        operators.arithmetic();
     }
 }
---
+++ src/com/company/operators.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class operators {
+
+    public static void arithmetic() {
+        int a = 1;
+        int b = 2;
+
+        int sum = a + b;
+        System.out.println("Sum of 1 and 2: " + sum);
+
+        int div =  a / b;
+        System.out.println("int division: " +div);
+
+        float c = 2F;
+        float d = 4F;
+
+        float fDiv = c / d;
+        System.out.println("float division: " + fDiv);
+    }
+}
