7bc27f5710276d1c53cd2ed44a250cbf650e35d5
Gitea <gitea@fake.local>
2022-02-27 02:25:36 +0530

    conditional

---
+++ src/com/company/conditional.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class conditional {
+
+    public static void ifElse() {
+        boolean isSunUp = false;
+        if (isSunUp == true) {
+            System.out.println("It's Day!");
+        } else {
+            System.out.println("It's Night!");
+        }
+    }
+}
